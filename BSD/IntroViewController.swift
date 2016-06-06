42de5f7c61a7ae6a6837ef61fd9636f48a2c332f
Victor Yurkin <viy2003@pc171816.med.cornell.edu>
2016-06-06 16:22:21 -0400

    General updates.

---
+++ BSD/IntroViewController.swift
@@ -0,0 +1,24 @@
+//
+//  IntroViewController.swift
+//  BSD
+//
+//  Created by Victor Yurkin on 6/6/16.
+//  Copyright © 2016 WCM. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
